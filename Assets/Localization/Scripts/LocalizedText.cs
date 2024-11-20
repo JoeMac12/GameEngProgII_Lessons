2f0b9f149ddfae9b94e797e93e3a393913f41688
Joseph <144450534+JoeMac12@users.noreply.github.com>
2024-11-20 14:57:20 -0400

    Localization and data

---
+++ Assets/Localization/Scripts/LocalizedText.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour
+{
+    [SerializeField] private string tableReference;
+    [SerializeField] private string localizationKey;
+
+    private LocalizedString localizedStr;
+    private Text textComponent;
+
+
+
+
+    void Start()
+    {
+        textComponent = GetComponent<Text>();
+        localizedStr = new LocalizedString { TableReference = tableReference, TableEntryReference = localizationKey };
+        LocalizationSettings.SelectedLocaleChanged += UpdateText;
+
+        var frenchLocale = LocalizationSettings.AvailableLocales.GetLocale("en"); // Start in en or fr
+        LocalizationSettings.SelectedLocale = frenchLocale;
+        UpdateText(frenchLocale);
+    }
+
+    private void OnDestroy()
+    {
+        LocalizationSettings.SelectedLocaleChanged -= UpdateText;
+    }
+
+    void UpdateText(Locale locale)
+    {
+        textComponent.text = localizedStr.GetLocalizedString(); // Translation logic
+    }
+}
---
+++ Assets/Localization/Scripts/Reader.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Language
+{
+    public string lang;
+    public string title;
+    public string play;
+    public string quit;
+    public string options;
+    public string credits;
+}
+
+public class LanguageData
+{
+    public Language[] languages;
+}
+
+public class Reader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    public string currentLanguage;
+    private LanguageData languageData;
+
+    public Text titleText;
+    public Text playText;
+    public Text optionsText;
+    public Text quitText;
+    public Text creditsText;
+
+    private void Start()
+    {
+        languageData = JsonUtility.FromJson<LanguageData>(jsonFile.text);
+        SetLanguage(currentLanguage);
+    }
+
+    public void SetLanguage(string newLanguage)
+    {
+        foreach (Language lang in languageData.languages)
+        {
+            if (lang.lang.ToLower() == newLanguage.ToLower())
+            {
+                titleText.text = lang.title;
+                playText.text = lang.play;
+                optionsText.text = lang.options;
+                quitText.text = lang.quit;
+                creditsText.text = lang.credits;
+                return;
+            }
+        }
+    }
+}
+
